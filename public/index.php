1a512cef37407c53dd861486f62d97a08ec50f31
Dwi Romadon <dwi.13421054@student.ubl.ac.id>
2025-04-30 12:01:27 +0700

    Add public/index.php 1745989285788

---
+++ public/index.php
@@ -0,0 +1,36 @@
+<?php
+// Autoload core classes and controllers
+spl_autoload_register(function ($class) {
+    $paths = [
+        __DIR__ . '/../core/' . $class . '.php',
+        __DIR__ . '/../app/controllers/' . $class . '.php',
+        __DIR__ . '/../app/models/' . $class . '.php',
+    ];
+    foreach ($paths as $path) {
+        if (file_exists($path)) {
+            require_once $path;
+            return;
+        }
+    }
+});
+
+// Simple routing based on URL parameter 'url'
+$url = isset($_GET['url']) ? rtrim($_GET['url'], '/') : 'home/index';
+$urlSegments = explode('/', $url);
+
+$controllerName = ucfirst($urlSegments[0]) . 'Controller';
+$action = isset($urlSegments[1]) ? $urlSegments[1] : 'index';
+$params = array_slice($urlSegments, 2);
+
+if (class_exists($controllerName)) {
+    $controller = new $controllerName();
+    if (method_exists($controller, $action)) {
+        call_user_func_array([$controller, $action], $params);
+    } else {
+        http_response_code(404);
+        echo "Action '$action' not found.";
+    }
+} else {
+    http_response_code(404);
+    echo "Controller '$controllerName' not found.";
+}
