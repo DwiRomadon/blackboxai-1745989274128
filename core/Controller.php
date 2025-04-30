75297b287490aa8a176b9357249406242588ec93
Dwi Romadon <dwi.13421054@student.ubl.ac.id>
2025-04-30 12:39:06 +0700

    Add core/Controller.php 1745991545112

--- core/Controller.php
+++ core/Controller.php
@@ -9,7 +9,17 @@ class Controller {
         if (strpos($url, '/') !== 0) {
             $url = '/' . $url;
         }
-        header("Location: " . $url);
+        // Build absolute URL
+        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+        $host = $_SERVER['HTTP_HOST'];
+        $absoluteUrl = $protocol . $host . $url;
+
+        // Clear output buffer
+        if (ob_get_length()) {
+            ob_end_clean();
+        }
+
+        header("Location: " . $absoluteUrl);
         exit;
     }
 }
